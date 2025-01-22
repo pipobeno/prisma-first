aad353b12c2dbd2f716aaf0de45cc1bf60017917
pipobeno <noura.khadraoui13003@gmail.com>
2025-01-22 19:15:05 +0100

    finish

--- router/bookRouter.js
+++ router/bookRouter.js
@@ -8,33 +8,97 @@ const bookRouter = require('express').Router();
 
 bookRouter.post("/addBook", authguard, async (req, res) => {
     try {
-        const {title, author} = req.body;
+        const { title, author } = req.body;
         const book = await prisma.book.create({
             data: {
-                title, 
+                title,
                 author,
                 userId: req.session.user.id
             }
         })
         res.redirect("/");
     } catch (error) {
-        res.render("/pages/index.html.twig", {error: error});
+        res.render("/pages/index.html.twig", { error: error });
     }
 
 })
 
-bookRouter.get("/deleteBook:/id", authguard, async (req, res) => {
-    try {
+bookRouter.get("/deleteBook/:id", authguard, async (req, res) => {
+    try{
         const deleteBook = await prisma.book.delete({
-            where: {
-                id: parseInt{req.params.id}
+            where:{
+                id: parseInt(req.params.id),
+                userId: req.session.user.id
             }
         })
-        
-    } catch (error) {
-        res.render("/pages/index.html/twig", {error: error})
+        res.redirect("/")
+    }
+    catch(error){
+        res.render("/pages/index.html.twig", {error: error});
     }
 })
+bookRouter.get("/editBook/:id", authguard, async (req, res) => {
+    try{
+        const book = await prisma.book.findFirst({
+            where:{
+                id: parseInt(req.params.id),
+                userId: req.session.user.id
+            }
+        })
+        const books = await prisma.book.findMany({
+            where:{
+                userId: req.session.user.id
+            }
+        })
 
-bookRouter.get("/editBook/id")
+        res.render("pages/index.html.twig", {
+            book, 
+            title:"Accueil", 
+            user: req.session.user, 
+            books
+        })
+    }
+    catch(error){
+        res.render("pages/index.html.twig", {error: error});
+    }   
+})
+
+bookRouter.post("/editBook/:id", authguard, async (req, res) => {
+    try{
+        const {title, author} = req.body;
+        if(title.match(/^[a-zA-Z0-9]+$/) && author.match(/^[a-zA-Z0-9]+$/)){
+            const book = await prisma.book.update({
+                where:{
+                    id: parseInt(req.params.id),
+                    userId: req.session.user.id
+                },
+                data:{
+                    title: req.body.title,
+                    author: req.body.author
+                }
+            })
+            res.redirect("/")
+        }
+        else throw ({error: "Le titre et l'auteur ne peuvent contenir que des lettres et des chiffres"});
+    }
+    catch(error){
+        console.log(error);
+        
+        res.render("pages/index.html.twig", {
+            user:req.session.user, 
+            error: error, 
+            books : await prisma.book.findMany({
+                where:{
+                    userId: req.session.user.id
+                }
+            }),
+            book: await prisma.book.findFirst({
+                where:{
+                    id: parseInt(req.params.id),
+                    userId: req.session.user.id
+                }
+            })
+        });
+    }
+})
 module.exports = bookRouter;
